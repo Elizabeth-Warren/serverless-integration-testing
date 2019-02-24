fdc9adbc187264a1a11cfe53a777c204cf3865df
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-02-23 20:25:00 -0500

    Trying a new idea with a bash script

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM node:8.10-stretch
 
+COPY bin/script.sh /usr/local/bin/sls-int-test.sh
+RUN chmod +x /usr/local/bin/sls-int-test.sh
+
 RUN npm install -g serverless
---
+++ bin/script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+$STAGE = ${$GITHUB_SHA:0:7}
+$COMMAND = $1;
+
+main() {
+  if [$COMMAND = 'create-stack'] then
+    sls deploy --stage $STAGE
+  elif [$COMMAND = 'delete-stack'] then
+    sls remove --stage $STAGE
+  else
+    echo "Invalid command supplied; must be one of [create-stack, delete-stack]."
+    exit 1
+  fi
+}
+
+main
